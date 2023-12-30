10c33910c57c52ebd5f432ff2004690eb6eafbfc
nazem0_home_pc <mohamednazem09@gmail.com>
2023-12-30 16:44:33 +0200

    bootstarp & ngx bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ReportComponent } from './components/report/report.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "report/:id", component: ReportComponent }
+];
