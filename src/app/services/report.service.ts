2f3d9f19d6cf83d7d46154ac7e759c53fa10514c
Samer Said <supsamer2009@icloud.com>
2023-12-31 20:44:28 +0200

    Add Report Service

--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Attachment } from '../models/attachment';
 import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
-  constructor() { }
+  constructor(private HttpClient: HttpClient) { }
+
   getAttachmentPath(attachment: Attachment) {
     return `${environment.server}/report/${attachment.reportId}/${attachment.name}`
   }
+
+  add(report: any) {
+    return this.HttpClient.post(`${environment.server}/Report`, report)
+  }
+
 }
