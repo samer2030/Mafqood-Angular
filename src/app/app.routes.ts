ea9626043fbc5fba7f9f1c77aa8f0c3aaabdf305
Samer Said <supsamer2009@icloud.com>
2023-12-31 20:44:50 +0200

    add report route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ReportComponent } from './components/report/report.component';
 import { NgModule } from '@angular/core';
+import { AddReportComponent } from './components/add-report/add-report.component';
 
 export const routes: Routes = [
-    { path: "report/:id", component: ReportComponent }
+    { path: "report/:id", component: ReportComponent },
+    { path: 'add', component: AddReportComponent }
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
